8bcbfab73521805e5cceeaf1766fc1f48a955a93
Olivier <olboussac@gmail.com>
2024-04-28 18:51:09 +0200

    chore : get name and description of last updated

---
+++ PSS/src/app/latest-manga-api-en.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class LatestMangaAPIENService {
+
+  constructor(private http: HttpClient) { }
+
+  baseUrl = "https://api.mangadex.org"
+
+  getLastMangaUpdated(): Observable<any[]> {    
+    return this.http.get<any[]>(this.baseUrl+"/manga?limit=20&availableTranslatedLanguage%5B%5D=en&availableTranslatedLanguage%5B%5D=fr&order%5BlatestUploadedChapter%5D=desc");
+  }
+
+}
