785fbd57490ed0b3ddb6c06af0ba2270f6021238
olivier <boussaco@3il.fr>
2024-05-10 03:13:11 +0200

    creation du backend

---
+++ PSS/PSS-backend/src/main/kotlin/com/pss/PSS/PssApplication.kt
@@ -0,0 +1,11 @@
+package com.pss.PSS
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PssApplication
+
+fun main(args: Array<String>) {
+	runApplication<PssApplication>(*args)
+}
---
+++ PSS/PSS-backend/src/test/kotlin/com/pss/PSS/PssApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pss.PSS
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PssApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
